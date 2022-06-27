a3c4ab71ee0289685f5172201683510a931392c6
NewtonOmbese <newtonombese1@gmail.com>
2022-06-27 06:52:11 +0300

    enabling calculation and has been successful

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
-let billAmount = document.getElementById("bill")
-let tipPercentage = document.getElementById("percentTip")
-let tipAmount = document.getElementById("tipAmount")
-let totalAmount = document.getElementById("total")
-let calculateAmount = document.getElementById("compute")
+let billAmount = document.getElementById("bill");
+let tipPercentage = document.getElementById("percentTip");
+let tipAmount = document.getElementById("tipAmount");
+let totalAmount = document.getElementById("total");
+let calculateAmount = document.getElementById("compute");
+let amountingTip;
+let amountingTotal;
 
+
+calculateAmount.addEventListener('click', ()=>{
+    let bills = billAmount.value;
+    let percentagingTip = tipPercentage.value;
+    amountingTip = (bills * percentagingTip) / 100;
+    console.log(amountingTip)
+    tipAmount.value = amountingTip
+    amountingTotal = parseInt(bills) + parseInt(amountingTip)
+    console.log(amountingTotal)
+    totalAmount.value = amountingTotal
+})
