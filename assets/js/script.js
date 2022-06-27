d197f0f0e6e76ae7dd95bf297f30e07e665b1ef8
NewtonOmbese <newtonombese1@gmail.com>
2022-06-27 06:26:37 +0300

    declaring variables with the dom calling

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+let billAmount = document.getElementById("bill")
+let tipPercentage = document.getElementById("percentTip")
+let tipAmount = document.getElementById("tipAmount")
+let totalAmount = document.getElementById("total")
+let calculateAmount = document.getElementById("compute")
+
